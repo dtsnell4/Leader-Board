7b7ef087a1dd209760a51c9ccfed299b7c8debc2
dtsnell4 <dtsnell4@yahoo.com>
2019-01-20 13:46:38 -0500

    Updates

---
+++ precache-manifest.3149a2ec0558be8375f4a9c5bb6f3853.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Leader-Board/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "36d23ae4278007b65c58",
+    "url": "/Leader-Board/static/css/main.177249c4.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Leader-Board/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "efd638c16dd137777a5e",
+    "url": "/Leader-Board/static/js/1.efd638c1.chunk.js"
+  },
+  {
+    "revision": "f94c9ffcddab5e014a74",
+    "url": "/Leader-Board/static/js/runtime~main.f94c9ffc.js"
+  },
+  {
+    "revision": "dc72d69ff4a080d64b7f5d14734d2e83",
+    "url": "/Leader-Board/static/media/pga_logo.dc72d69f.png"
+  },
+  {
+    "revision": "36d23ae4278007b65c58",
+    "url": "/Leader-Board/static/js/main.36d23ae4.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Leader-Board/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Leader-Board/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Leader-Board/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "efd638c16dd137777a5e",
+    "url": "/Leader-Board/static/css/1.dbba9331.chunk.css"
+  },
+  {
+    "revision": "1881969e9c2df5aa88c7c49e34a74791",
+    "url": "/Leader-Board/index.html"
+  }
+];
